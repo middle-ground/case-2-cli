9b9c6201187cd542bd568639de21698eea1bb0a5
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 04:15:51 +0700

    Clean code and add space code

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ console.log("Receiver email is required!");
 console.log("Create template email");
 console.log("Add logic between template and data");
 console.log("Implement the code");
+console.log("Funny things! its just a demo");

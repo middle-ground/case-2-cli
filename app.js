ca4bad005ca8df64393b905c61d169d0b4bbfbd4
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 04:14:54 +0700

    Add feature email

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Initial commit for project ABC");
+console.log("To create feature send email");
+console.log("Receiver email is required!");
+console.log("Create template email");
+console.log("Add logic between template and data");
+console.log("Implement the code");

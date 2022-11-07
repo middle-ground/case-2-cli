60933e859cb316fc734589e78cacbd9c52d6d862
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 04:12:29 +0700

    Algoritma to create feature

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Initial commit for project ABC");
+console.log("Algoritma of rendering graphic");
+console.log("1. Get data information in raw but calculable");
+console.log("2. Calculate data with measurement formula");
+console.log("3. Set diagram of view");
+console.log("4. Create logic flow for interface");
+console.log("5. Choose color and style");
